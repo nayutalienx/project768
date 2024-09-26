764f446afd79052d7254cd98c50f5bbb7c13dcf9
nayutalienx <nayutalienx@gmail.com>
2024-09-27 06:47:25 +0700

    Setted normal collission layers

--- scripts/ladder/Ladder.cs
+++ scripts/ladder/Ladder.cs
@@ -3,91 +3,81 @@ using System;
 
 public partial class Ladder : Node2D
 {
+    [Signal]
+    public delegate void LadderInteractEventHandler(
+        LadderEvent ladderEvent
+    );
 
-	[Signal]
-	public delegate void LadderInteractEventHandler(
-		LadderEvent ladderEvent
-	);
+    public override void _Ready()
+    {
+        Area2D moveUpArea = GetNode<Area2D>("move_up_area");
+        moveUpArea.BodyEntered += MoveUpZoneEntered;
+        moveUpArea.BodyExited += MoveUpZoneExited;
 
-	private StaticBody2D ladderTop;
+        Area2D moveDownArea = GetNode<Area2D>("move_down_area");
 
-	public override void _Ready()
-	{
-		ladderTop = GetNode<StaticBody2D>("top");
+        moveDownArea.BodyEntered += MoveDownZoneEntered;
+        moveDownArea.BodyExited += MoveDownZoneExited;
+    }
 
-		Area2D moveUpArea = GetNode<Area2D>("move_up_area");
-		moveUpArea.BodyEntered += MoveUpZoneEntered;
-		moveUpArea.BodyExited += MoveUpZoneExited;
+    public void MoveUpZoneEntered(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            EmitSignal(
+                SignalName.LadderInteract,
+                new LadderEvent
+                {
+                    NearLadder = true,
+                    Position = Position,
+                    Snap = LadderEvent.LadderSnap.BOTTOM_SNAP
+                });
+        }
+    }
 
-		Area2D moveDownArea = GetNode<Area2D>("move_down_area");
-
-		moveDownArea.BodyEntered += MoveDownZoneEntered;
-		moveDownArea.BodyExited += MoveDownZoneExited;
-	}
-
-	public void MoveUpZoneEntered(Node2D body)
-	{
-		if (body.IsInGroup("player"))
-		{
-			EmitSignal(
-				SignalName.LadderInteract,
-				new LadderEvent
-				{
-					NearLadder = true,
-					Position = Position,
-					LadderTop = ladderTop,
-					Snap = LadderEvent.LadderSnap.BOTTOM_SNAP
-				});
-		}
-	}
-
-	public void MoveUpZoneExited(Node2D body)
-	{
-		if (body.IsInGroup("player"))
-		{
-			EmitSignal(
-				SignalName.LadderInteract,
-				new LadderEvent
-				{
-					NearLadder = false,
-					Position = Position,
-					LadderTop = ladderTop,
-					Snap = LadderEvent.LadderSnap.BOTTOM_SNAP
-				});
-		}
-	}
+    public void MoveUpZoneExited(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            EmitSignal(
+                SignalName.LadderInteract,
+                new LadderEvent
+                {
+                    NearLadder = false,
+                    Position = Position,
+                    Snap = LadderEvent.LadderSnap.BOTTOM_SNAP
+                });
+        }
+    }
 
 
-	public void MoveDownZoneEntered(Node2D body)
-	{
-		if (body.IsInGroup("player"))
-		{
-			EmitSignal(
-				SignalName.LadderInteract,
-				new LadderEvent
-				{
-					NearLadder = true,
-					Position = Position,
-					LadderTop = ladderTop,
-					Snap = LadderEvent.LadderSnap.TOP_SNAP
-				});
-		}
-	}
+    public void MoveDownZoneEntered(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            EmitSignal(
+                SignalName.LadderInteract,
+                new LadderEvent
+                {
+                    NearLadder = true,
+                    Position = Position,
+                    Snap = LadderEvent.LadderSnap.TOP_SNAP
+                });
+        }
+    }
 
-	public void MoveDownZoneExited(Node2D body)
-	{
-		if (body.IsInGroup("player"))
-		{
-			EmitSignal(
-				SignalName.LadderInteract,
-				new LadderEvent
-				{
-					NearLadder = false,
-					Position = Position,
-					LadderTop = ladderTop,
-					Snap = LadderEvent.LadderSnap.TOP_SNAP
-				});
-		}
-	}
-
+    public void MoveDownZoneExited(Node2D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            EmitSignal(
+                SignalName.LadderInteract,
+                new LadderEvent
+                {
+                    NearLadder = false,
+                    Position = Position,
+                    Snap = LadderEvent.LadderSnap.TOP_SNAP
+                });
+        }
+    }
 }
