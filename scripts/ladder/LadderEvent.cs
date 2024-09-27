98162df4bea254721d3a381a512154950445fb98
nayutalienx <nayutalienx@gmail.com>
2024-09-27 12:47:17 +0700

    Added one-way-collission

--- scripts/ladder/LadderEvent.cs
+++ scripts/ladder/LadderEvent.cs
@@ -1,17 +1,8 @@
-
 using Godot;
 using System;
 
 public partial class LadderEvent : GodotObject
 {
-    public enum LadderSnap
-    {
-        TOP_SNAP,
-        BOTTOM_SNAP
-    }
-
     public bool NearLadder { get; set; }
     public Vector2 Position { get; set; }
-    public LadderSnap Snap { get; set; }
-
 }
