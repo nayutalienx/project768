e12aa33350f52d3476e80b4d1b3f988258a94ebe
nayutalienx <nayutalienx@gmail.com>
2024-10-18 03:10:05 +0800

    added particles for cannon balls

--- scripts/game_entity/landscape/cannon/cannon_ball/CannonBall.cs
+++ scripts/game_entity/landscape/cannon/cannon_ball/CannonBall.cs
@@ -1,4 +1,5 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 using project768.scripts.rewind.entity;
 using project768.scripts.state_machine;
 
@@ -21,9 +22,32 @@ public partial class CannonBall : Area2D,
     public State<CannonBall, State>[] States { get; set; }
     public StateChanger<CannonBall, State> StateChanger { get; set; }
     public int RewindState { get; set; }
+    public Sprite2D Sprite { get; set; }
+    public GpuParticles2D Particles { get; set; }
+
+    public bool BallHidden
+    {
+        get => ballHidden;
+        set
+        {
+            if (value)
+            {
+                HideBall();
+            }
+            else
+            {
+                ShowBall();
+            }
+        }
+    }
+
+    private bool ballHidden = true;
 
     public override void _Ready()
     {
+        Sprite = GetNode<Sprite2D>("Sprite2D");
+        Particles = GetNode<GpuParticles2D>("GPUParticles2D");
+
         States = new State<CannonBall, State>[]
         {
             new WaitState(this, State.Wait),
@@ -39,6 +63,20 @@ public partial class CannonBall : Area2D,
         };
 
         StateChanger.ChangeState(State.Wait);
+    }
+
+    public void HideBall()
+    {
+        ballHidden = true;
+        Sprite.Hide();
+        Particles.SetEmitting(false);
+    }
+
+    public void ShowBall()
+    {
+        ballHidden = false;
+        Sprite.Show();
+        Particles.SetEmitting(true);
     }
 
     public override void _PhysicsProcess(double delta)
@@ -58,5 +96,14 @@ public partial class CannonBall : Area2D,
 
     public void OnRewindSpeedChanged(int speed)
     {
+        if (speed == 0)
+        {
+            Particles.SetEmitting(false);
+        }
+        else
+        {
+            Particles.SetEmitting(true);
+            Particles.SpeedScale = Mathf.Abs(speed);
+        }
     }
 }
