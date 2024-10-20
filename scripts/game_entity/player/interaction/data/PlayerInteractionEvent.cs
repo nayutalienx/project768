aa17c2754b908ab763ace9ddfd0cfcef30dc7894
nayutalienx <nayutalienx@gmail.com>
2024-10-20 21:18:52 +0800

    refactoring ladder interaction

--- scripts/game_entity/player/interaction/data/PlayerInteractionEvent.cs
+++ scripts/game_entity/player/interaction/data/PlayerInteractionEvent.cs
@@ -5,6 +5,7 @@ namespace project768.scripts.player.interaction;
 
 public class PlayerInteractionEvent : InteractionEvent<PlayerInteraction>
 {
+    public bool JoinedLadderArea { get; set; }
     public Vector2 Ladder { get; set; }
     public Key Key { get; set; }
 
