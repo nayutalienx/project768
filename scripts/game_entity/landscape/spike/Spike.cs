e04b596aa3237b540c2d185ef404767070d91a20
nayutalienx <nayutalienx@gmail.com>
2024-10-27 03:50:11 +0800

    wall tile

--- scripts/game_entity/landscape/spike/Spike.cs
+++ scripts/game_entity/landscape/spike/Spike.cs
@@ -1,29 +1,17 @@
+using System.Transactions;
 using Godot;
+using project768.scripts.game_entity.common;
 using project768.scripts.game_entity.npc.enemy.interaction.data;
 using project768.scripts.player;
 using project768.scripts.player.interaction;
 
-public partial class Spike : Line2D
+public partial class Spike : DynamicSprite
 {
     public override void _Ready()
     {
-        CollisionPolygon2D collisionPolygon2D = GetNode<CollisionPolygon2D>("Area2D/CollisionPolygon2D");
+        InitDynamicSprite();
 
-        var linePoints = GetPoints();
-
-        float height = 10;
-
-        Vector2[] polyPoints = new Vector2[4]
-        {
-            linePoints[0],
-            linePoints[1],
-            linePoints[0] + new Vector2(0, height),
-            linePoints[1] + new Vector2(0, height),
-        };
-
-        collisionPolygon2D.Polygon = polyPoints;
-
-        GetNode<Area2D>("Area2D").BodyEntered += body =>
+        GetNode<Area2D>("Shape").BodyEntered += body =>
         {
             if (body is Player player)
             {
