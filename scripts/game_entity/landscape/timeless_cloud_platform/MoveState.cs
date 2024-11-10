f1c0c2a8f57f860c2e280ebe903a57b49419e168
nayutalienx <nayutalienx@gmail.com>
2024-11-10 20:44:15 +0700

    added timeless cannon and cloud platform ^& added world 2 2

---
+++ scripts/game_entity/landscape/timeless_cloud_platform/MoveState.cs
@@ -0,0 +1,26 @@
+﻿using project768.scripts.game_entity.landscape.cloud_platform;
+using project768.scripts.state_machine;
+
+namespace project768.scripts.game_entity.landscape.timeless_cloud_platform;
+
+public class MoveState : State<TimelessCloudPlatform, TimelessCloudPlatform.State>
+{
+    public MoveState(TimelessCloudPlatform entity, TimelessCloudPlatform.State stateEnum) : base(entity, stateEnum)
+    {
+    }
+
+    public override void EnterState(TimelessCloudPlatform.State prevState)
+    {
+        Entity.CallDeferred(nameof(Entity.ShowCloud));
+    }
+
+    public override void PhysicProcess(double delta)
+    {
+        Entity.GlobalTransform = Entity.GlobalTransform.Translated(Entity.Direction * (float) (Entity.Speed * delta));
+
+        if (Entity.RayCast2D.IsColliding())
+        {
+            Entity.StateChanger.ChangeState(TimelessCloudPlatform.State.Wait);
+        }
+    }
+}
---
+++ scripts/game_entity/landscape/timeless_cloud_platform/WaitState.cs
@@ -0,0 +1,16 @@
+﻿using project768.scripts.state_machine;
+
+namespace project768.scripts.game_entity.landscape.timeless_cloud_platform;
+
+public class WaitState : State<TimelessCloudPlatform, TimelessCloudPlatform.State>
+{
+    public WaitState(TimelessCloudPlatform entity, TimelessCloudPlatform.State stateEnum) : base(entity, stateEnum)
+    {
+    }
+
+    public override void EnterState(TimelessCloudPlatform.State prevState)
+    {
+        Entity.CallDeferred(nameof(Entity.HideCloud));
+    }
+    
+}
