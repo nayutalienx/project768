b8f348c5b13adcb8d7955d16c70d045738bf3280
nayutalienx <nayutalienx@gmail.com>
2024-12-06 14:17:32 +0800

    added spacetime locked door

---
+++ scripts/game_entity/landscape/spacetime_door/LockedState.cs
@@ -0,0 +1,63 @@
+﻿using project768.scripts.common;
+using project768.scripts.player;
+using project768.scripts.player.interaction;
+using project768.scripts.state_machine;
+
+namespace project768.scripts.game_entity.landscape.spacetime_door;
+
+public class LockedState : State<SpacetimeLockedDoor, SpacetimeLockedDoor.State>
+{
+    public LockedState(SpacetimeLockedDoor entity, SpacetimeLockedDoor.State stateEnum) : base(entity, stateEnum)
+    {
+    }
+
+
+    public override void EnterState(SpacetimeLockedDoor.State prevState)
+    {
+        Entity.CollisionShape2D.SetDeferred("disabled", false);
+        Entity.LockArea.SetDeferred("monitoring", true);
+        
+        Entity.AnimationPlayer.SetCurrentAnimation("DoorOpen");
+        Entity.AnimationPlayer.SpeedScale = 0.0f;
+        Entity.AnimationPlayer.Seek(0.0f, true);
+    }
+
+    public override void Process(double delta)
+    {
+        if (Entity.TrackEnemies)
+        {
+            bool allDead = true;
+
+            foreach (EnemySpacetime entityEnemy in Entity.Enemies)
+            {
+                if (entityEnemy.CurrentState.StateEnum != EnemySpacetime.State.Death)
+                {
+                    allDead = false;
+                    break;
+                }
+            }
+
+            if (allDead)
+            {
+                Entity.StateChanger.ChangeState(SpacetimeLockedDoor.State.Unlocked);
+            }
+        }
+    }
+
+    public override void OnBodyEntered(CollisionBody body)
+    {
+        if (body.Body is project768.scripts.player.Player player &&
+            (
+                player.InteractionContext.KeyContext.HasKey ||
+                player.InteractionContext.TimelessKeyContext.HasKey
+            ))
+        {
+            Entity.PlayerPositionWhenDoorUnlocked = Player.Instance.GlobalPosition;
+
+            player.Interactor.Interact(
+                new PlayerInteractionEvent(PlayerInteraction.UnlockedDoor)
+            );
+            Entity.StateChanger.ChangeState(SpacetimeLockedDoor.State.Unlocked);
+        }
+    }
+}
---
+++ scripts/game_entity/landscape/spacetime_door/UnlockedState.cs
@@ -0,0 +1,44 @@
+﻿using Godot;
+using project768.scripts.player;
+using project768.scripts.rewind;
+using project768.scripts.state_machine;
+
+namespace project768.scripts.game_entity.landscape.spacetime_door;
+
+public class UnlockedState : State<SpacetimeLockedDoor, SpacetimeLockedDoor.State>
+{
+    public UnlockedState(SpacetimeLockedDoor entity, SpacetimeLockedDoor.State stateEnum) : base(entity, stateEnum)
+    {
+    }
+    
+    public override void EnterState(SpacetimeLockedDoor.State prevState)
+    {
+        Entity.CollisionShape2D.SetDeferred("disabled", true);
+        Entity.LockArea.SetDeferred("monitoring", false);
+        
+        Entity.AnimationPlayer.SetCurrentAnimation("DoorOpen");
+        Entity.AnimationPlayer.SpeedScale = 0.0f;
+        Entity.AnimationPlayer.Seek(0.0f, true);
+    }
+    
+    public override void Process(double delta)
+    {
+        float animationProgress = SpacetimeRewindPlayer.CalculateTimelineProgress(
+            Player.Instance.GlobalPosition.X,
+            Entity.PlayerPositionWhenDoorUnlocked.X,
+            Entity.PlayerPositionWhenDoorUnlocked.X + Entity.OpenDoorTimelineLength
+        );
+        float animationPosition = (float) Mathf.Lerp(
+            0.0f,
+            Entity.AnimationPlayer.GetCurrentAnimationLength(),
+            Mathf.Clamp(animationProgress, 0.0f, 0.98f)
+        );
+        Entity.AnimationPlayer.Seek(animationPosition, true);
+
+        if (Player.Instance.GlobalPosition.X < Entity.PlayerPositionWhenDoorUnlocked.X)
+        {
+            Entity.StateChanger.ChangeState(SpacetimeLockedDoor.State.Locked);
+        }
+    }
+    
+}
